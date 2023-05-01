8ffe8b4cb042ccb69dfb4e8f19746563ce2642ad
Lexmark444 <104227345+Lexmark444@users.noreply.github.com>
2023-05-01 17:02:24 -0400

    backend added

---
+++ Node API/models/Product.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: [true, "Please enter a product name"], unique: true},
+        desc: { type: String, required: false },
+        img: { type: String, required: false },
+        categories: {type: Array},
+        price: { type: Number, required: true, default: 1.00 },
+    },
+    { timestamps: true } 
+
+)
+
+module.exports = mongoose.model('Product', ProductSchema);
+
---
+++ Node API/routes/stripe.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+
+router.post("/payment", (req, res) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd",
+    }, ( stripeErr, stripeRes ) => {
+        if(stripeErr){
+            res.status(500).json(stripeErr)
+        } else {
+            res.status(200).json(stripeRes)
+        }
+    })
+})
+
+
+// const items = req.body // => [{ _id: '001', quantity: 3 }, ...]
+// const itemIds = items.map(item => item._id);
+// const foundProducts = await Product.find({ _id: { $in: itemIds } }) // => [{ _id, name, imageURL, price }, ...]
+
+// const total = items.reduce((runningTotal, item) => {
+//   const { price } = foundProducts.find(prod => prod._id === item._id)
+//   const totalItemPrice = price * item.quantity;
+//   return runningTotal + totalItemPrice 
+// }, 0)
+
+module.exports = router;
