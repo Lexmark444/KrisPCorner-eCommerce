e2e3a297c83471fd3f849479f9d48149b6807e44
Lexmark444 <104227345+Lexmark444@users.noreply.github.com>
2023-05-05 15:20:53 -0400

    TODO: Make it more responsive

--- eCommerce Frontend/src/pages/Login.jsx
+++ eCommerce Frontend/src/pages/Login.jsx
@@ -1,11 +1,105 @@
 import styled from "styled-components"
+import loginbackground from "../assets/images/loginbackground.jpg"
 
-const Container = styled.div``
+const Container = styled.div`
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  
+  background-image: linear-gradient(
+    rgba(255,255,255,0.3),
+    rgba(255,255,255,0.3)
+    ),
+    url(${loginbackground});
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px 20px 15px;
+  background-color: white;
+  opacity: 90%;
+  border-radius: 3px;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 400;
+  color: teal;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+
+`
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+const Button = styled.button`
+  width: 33%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  border-radius: 3px;
+
+  &:hover {
+    background-color: pink;
+  }  
+`
+const LinkContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 7px 0px 0;
+
+`
+const Link = styled.a`
+  text-decoration: underline;
+  font-size: 12px;
+  color: teal;
+  cursor: pointer;
+
+  &:hover {
+    color: pink;
+  }
+`
 
 const Login = () => {
   return (
     <Container>
-      
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+
+          <Input placeholder="Username"/>
+          <Input placeholder="Password"/>
+          <ButtonContainer>
+            <Button>LOGIN</Button>
+          </ButtonContainer>
+          <LinkContainer>
+            <Link>Register</Link>
+            <Link>Forgot Password?</Link>
+          </LinkContainer>
+        </Form>
+      </Wrapper>
     </Container>
   )
 }
--- eCommerce Frontend/src/pages/Register.jsx
+++ eCommerce Frontend/src/pages/Register.jsx
@@ -1,11 +1,91 @@
 import styled from "styled-components"
+import registerbg from "../assets/images/registerbg.jpg"
 
-const Container = styled.div``
+const Container = styled.div`
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  
+  background-image: linear-gradient(
+    rgba(255,255,255,0.3),
+    rgba(255,255,255,0.3)
+    ),
+    url(${registerbg});
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background-color: white;
+  opacity: 90%;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 400;
+  color: teal;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`
+
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`
+
+const Button = styled.button`
+  width: 33%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  border-radius: 3px;
+
+  &:hover {
+    background-color: pink;
+  }
+`
 
 const Register = () => {
   return (
     <Container>
-      
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name"/>
+          <Input placeholder="Last Name"/>
+          <Input placeholder="Email"/>
+          <Input placeholder="Username"/>
+          <Input placeholder="Password"/>
+          <Agreement>By creating an account I consent to the processing of my personal data in accordance
+            with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <ButtonContainer>
+          <Button>SUBMIT</Button>
+          </ButtonContainer>
+        </Form>
+      </Wrapper>
     </Container>
   )
 }
